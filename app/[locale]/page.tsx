e869a2afd370d908a7d8191b44194caa19ce78d8
karim <karim.ghodbane@tanitweb.com>
2026-09-24 23:21:23 +0100

    MAJ

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -241,7 +241,7 @@ export default function LandingPage() {
             <div className={styles.restaurantInfoText}>
               <span>NOUS TROUVER</span>
               <strong>14 avenue de l'environnement</strong>
-              <small>L&apos;Aouina</small>
+              <small>L Aouina</small>
             </div>
           </div>
 
